909d44724938d1c4f7d7260ba9ad0e781558515a
Maddie Weber <maddieweber7@gmail.com>
2023-12-12 12:28:17 -0500

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
+
+fido = Dog("Fido")
+fido.name
+fido.get_adopted("Sophie")
+fido.owner
+
+snoopy = Dog("Snoopy")
+snoopy.name
